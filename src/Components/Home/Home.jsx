996037bb65e4b16a39f600a9c7ced3cda9dddb95
Thibaud Moreno <thibaudmoreno@macbook-air-van-thibaud.home>
2020-06-26 10:44:30 +0200

    [REACT] add components home + carousel

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Cryptomonnaies from "./Cryptomonnaies/Cryptomonnaies";
+import Echanges from "./Echanges/Echanges";
+import Revolutions from "./Revolution/Revolution";
+import Technologies from "./Technologie/Technologie";
 
 const Home = () => {
-  return <div>Hello home</div>;
+  const [crypto, setCrypto] = useState(true);
+  const [echange, setEchange] = useState(false);
+  const [revo, setRevo] = useState(false);
+  const [techno, setTechno] = useState(false);
+
+  const Crypto = () => {
+    setCrypto(true);
+    setEchange(false);
+    setRevo(false);
+    setTechno(false);
+  };
+
+  const Echange = () => {
+    setEchange(true);
+    setCrypto(false);
+    setRevo(false);
+    setTechno(false);
+  };
+
+  const Revolution = () => {
+    setRevo(true);
+    setCrypto(false);
+    setTechno(false);
+    setEchange(false);
+  };
+
+  const Technologie = () => {
+    setTechno(true);
+    setCrypto(false);
+    setEchange(false);
+    setRevo(false);
+  };
+
+  return (
+    <div>
+      {crypto ? <Cryptomonnaies /> : null}
+      {echange ? <Echanges /> : null}
+      {revo ? <Revolutions /> : null}
+      {techno ? <Technologies /> : null}
+      <button onClick={Crypto}>Crypto</button>
+      <button onClick={Echange}>Echanges</button>
+      <button onClick={Revolution}>Revo</button>
+      <button onClick={Technologie}>Techno</button>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Components/Notfound/Notfound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Notfound = () => {
+  return <div>Erreur de la page</div>;
+};
+
+export default Notfound;
