b3bcad28feda26aa4d85241e40c140ce39e48948
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-06-29 10:25:15 +0200

    [REACT] update path

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,28 +1,10 @@
-<<<<<<< HEAD
 import React from "react";
+import Header from "../Header/Header";
 import Cryptomonnaies from "./Cryptomonnaies/Cryptomonnaies";
 import Echanges from "./Echanges/Echanges";
 import Revolutions from "./Revolution/Revolution";
 import Technologies from "./Technologie/Technologie";
-
-const Home = () => {
-  return (
-    <div className="wrapper wrapper--home">
-      <div className="container">
-        <div className="contents">
-          <div className="carousel">
-            <Cryptomonnaies />
-            <Echanges />
-            <Revolutions />
-            <Technologies />
-=======
-import React from 'react';
-import Header from '../Header/Header';
-import Cryptomonnaies from './Cryptomonnaies/Cryptomonnaies';
-import Echanges from './Echanges/Echanges';
-import Revolutions from './Revolution/Revolution';
-import Technologies from './Technologie/Technologie';
-import Slider from 'react-slick';
+import Slider from "react-slick";
 
 const Home = () => {
   const settings = {
@@ -32,7 +14,7 @@ const Home = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    adaptiveHeight: true,
+    adaptiveHeight: true
   };
   return (
     <div className="home">
@@ -50,7 +32,6 @@ const Home = () => {
               </Slider>
             </div>
             <div className="illustration">ILLUSTRATION CSS</div>
->>>>>>> f640a47d9bf00e5102e781df43079a51880b4bd4
           </div>
           <div className="illustration"></div>
         </div>
